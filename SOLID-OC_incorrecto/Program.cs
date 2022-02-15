a11c77571ed4653171ab97d4f38d7787fabc5ca8
Behnder <mercado.nicolas@live.com>
2022-02-15 12:11:26 -0300

    first commit

---
+++ SOLID-OC_incorrecto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OC_incorrecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repositorio repositorio = new Repositorio();
+
+            repositorio.Actualizar();
+            repositorio.Cargar();
+            repositorio.Guardar();
+            repositorio.Quitar();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SOLID-OC_incorrecto/Repositorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OC_incorrecto
+{
+    class Repositorio
+    {
+        public void Cargar()
+        {
+            Console.WriteLine("Cargando datos para BD SQL");
+
+        }
+        public void Guardar()
+        {
+            Console.WriteLine("Guardando datos para BD SQL");
+        }
+        public void Actualizar()
+        {
+            Console.WriteLine("Actualizando datos para BD SQL");
+        }
+        public void Quitar()
+        {
+            Console.WriteLine("Eliminando datos para BD SQL");
+        }
+    }
+}
